2e79dac2814ac33439531499a67874a2100c1465
dqriakh <dashakhan3009@mail.ru>
2024-04-13 13:44:20 +0300

    united

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import Main from './pages/main';
+
+import { Main } from './pages/main'
+
+import './style/main.css'
+
 const App = () => {
   return (
-      </Main>
+    <Main />
   );
 };
+
 export default App;
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,68 +1,79 @@
-import React from 'react';
+import React from "react";
 
-const Main = () => {
+export const Main = () => {
   return (
     <>
-    <header className="header">
-    <div className="wrapper">
-        <div className="header-wrapper">
-        <div className="header-logo">
-            <a href="#" className="header-logo-link">
-                <img src="./img/svg/logo.svg" alt="Логотип. PsycologyTesting" className="header-logo-pic" />
-            </a>
-        </div>
-
-        <nav className="header-nav">
-            <ul className="nav-list">
-                <li><a className="link-contrast" href="#01">ТЕСТЫ</a></li>
-                <li><a className="link-contrast" href="#02">ЛИЧНЫЙ КАБИНЕТ</a></li>
-            </ul>
-        </nav>
-        </div>
-    </div>
-</header>
-
-<main className="main-content">
-    <section className="card1">
-        <img src="./img/svg/lotos.svg" alt="девушка в позе лотоса" />
-        <div className="wrapper">
-                <h1 className="h1">О САЙТЕ</h1>
-                <p className="text1">
-                    Добро пожаловать на сайт, 
-                    посвященный психологическим 
-                    тестам! Здесь вы найдете широкий 
-                    выбор тестов, которые помогут 
-                    вам лучше понять себя, свои 
-                    эмоции, поведение и отношения 
-                    с окружающими.
-                </p>
-        </div>
-    </section>
-    <section className="card1">
-        <h2 className="h">ОНЛАЙН ПСИХОЛОГИЧЕСКИЕ ТЕСТЫ</h2>
-        <p className="text2">
-            Психологические тесты – это мощный 
-            инструмент для самопознания. 
-            Они помогают выявить личностные черты, 
-            скрытые таланты, а также понять свои сильные 
-            и слабые стороны. 
-        </p>
-        <img className="girl" src="./img/svg/girl.svg" alt="Спокойная девушка"/>
-        <div className="link-button">
-            <a className="link-contrast-button" href="#03">К ТЕСТАМ!</a>
-        </div>
-    </section>
-    <section className="card1">
-        <h2 className="h">РЕГИСТРАЦИЯ И КОНСУЛЬТАЦИИ</h2>
-        <div className="content3">
-            <div className="link-button1">
-                <a className="link-contrast-button" href="#04">РЕГИСТРАЦИЯ</a>
+    <header class="header">
+        <div class="wrapper">
+            <div class="header-wrapper">
+            <img src="../img/svg/logo.svg" alt="Логотип. PsycologyTesting" class="header-logo-pic"/>
+            <nav class="header-nav">
+                <ul class="nav-list">
+                    <li><a class="link-contrast" href="#01">ТЕСТЫ</a></li>
+                    <li><a class="link-contrast" href="#02">ЛИЧНЫЙ КАБИНЕТ</a></li>
+                </ul>
+            </nav>
             </div>
-        <p className="text3">дает возможность: сохранять и просматривать результаты тестирований, записываться  к специалисту, следить за графиком консультаций</p>
         </div>
-        <img src="./img/svg/psyho.svg" alt="Консультация психолога" />
-    </section>
-</main>
+    </header>
+    
+    <main class="main-content">
+        <div class="wrapper">
+        <section class="card1">
+            <img src="../img/svg/lotos.svg" alt="девушка в позе лотоса" />
+            <div class="wrapper">
+                    <h1 class="h1">О САЙТЕ</h1>
+                    <p class="text1">
+                        Добро пожаловать на сайт, 
+                        посвященный психологическим 
+                        тестам! Здесь вы найдете широкий 
+                        выбор тестов, которые помогут 
+                        вам лучше понять себя, свои 
+                        эмоции, поведение и отношения 
+                        с окружающими.
+                    </p>
+            </div>
+        </section>
+        <section class="card1">
+            <h class="h">ОНЛАЙН ПСИХОЛОГИЧЕСКИЕ ТЕСТЫ</h>
+            <p class="text2">
+                Психологические тесты – это мощный 
+                инструмент для самопознания. 
+                Они помогают выявить личностные черты, 
+                скрытые таланты, а также понять свои сильные 
+                и слабые стороны. 
+            </p>
+            <img сlass="girl" src="../img/svg/girl.svg" alt="Спокойная девушка" />
+            <button class="link-button"> 
+                <a class="link-contrast-button" href="#03">К ТЕСТАМ!</a>
+            </button>
+        </section>
+        <section class="card1">
+            <h class="h">РЕГИСТРАЦИЯ И КОНСУЛЬТАЦИИ</h>
+            <div class="content3">
+                <button class="link-button1">
+                    <a class="link-contrast-button" href="#04">РЕГИСТРАЦИЯ</a>
+                </button>
+            <p class="text3">дает возможность: сохранять и просматривать результаты тестирований, записываться  к специалисту, следить за графиком консультаций</p>
+            </div>
+            <img src="../img/svg/psyho.svg" alt="Консультация психолога" />
+        </section>
+        </div>
+    </main>
+
+    <footer class="footer">
+        <div class="wrapper">
+            <div class="header-wrapper">
+            <img src="../img/svg/logo.svg" alt="Логотип. PsycologyTesting" class="header-logo-pic"/>
+            <div class="footer-wrapper">
+                <p class="text">created by</p>
+                <a href="https://github.com/sashunechko/dhs-testing">
+                    <img src="../img/svg/git.svg" alt="Логотип. Github" class="header-logo-pic"/>
+                </a>
+            </div>
+            </div>
+        </div>
+    </footer>
 </>
   );
 };
