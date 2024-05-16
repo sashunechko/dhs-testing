dfda5a9ac1169c11268941c10441dda0171ba342
dqriakh <dashakhan3009@mail.ru>
2024-05-16 22:13:11 +0300

    (#5) Fixed component "header"

--- src/components/header/header.styled.ts
+++ src/components/header/header.styled.ts
@@ -1,15 +1,6 @@
 import styled from "@emotion/styled";
 
-<<<<<<< HEAD
-export const Wrapper = styled.div`
-    max-width: 1295px;
-    margin: 0 auto;
-`;
-
-export const HeaderStyled = styled.header`
-=======
 export const HeaderBlock = styled.header`
->>>>>>> dac0d4f27d8fb3092f3cfa8a6ada9e8b0eff8ba3
     background-color: #374375;
     position: fixed;
     top:0;
@@ -17,14 +8,11 @@ export const HeaderBlock = styled.header`
     width: 100%;
 `;
 
-<<<<<<< HEAD
-=======
 export const Wrapper = styled.div`
     max-width: 1295px;
     margin: 0 auto;
 `;
 
->>>>>>> dac0d4f27d8fb3092f3cfa8a6ada9e8b0eff8ba3
 export const HeaderWrapper = styled.div`
     display: flex;
     flex-wrap:wrap;
@@ -32,20 +20,11 @@ export const HeaderWrapper = styled.div`
     justify-content: space-between;
 `;
 
-<<<<<<< HEAD
-export const HeaderList = styled.ul`
-    display: flex;
-    flex-wrap:wrap;
-`;
-
-export const StyledLink = styled.a`
-=======
 export const HeaderNav = styled.ul`
     display: flex;
     flex-wrap:wrap;
 `;
 export const LinkContrast = styled.a`
->>>>>>> dac0d4f27d8fb3092f3cfa8a6ada9e8b0eff8ba3
     font-family: 'Inter';
     font-weight: 500;
     font-size: 32px;
@@ -53,13 +32,8 @@ export const LinkContrast = styled.a`
     color: #fff;
     margin-right: 82px;
     text-decoration: none;
-<<<<<<< HEAD
-`;
-
-=======
     
     :hover, :focus, :active{
         opacity: .75;
     }
 `;
->>>>>>> dac0d4f27d8fb3092f3cfa8a6ada9e8b0eff8ba3
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,34 +1,10 @@
 import React from "react"
 
-<<<<<<< HEAD
-import { HeaderStyled, Wrapper, HeaderWrapper, HeaderList, StyledLink} from "./header.styled";
-=======
 import './header.styled';
->>>>>>> dac0d4f27d8fb3092f3cfa8a6ada9e8b0eff8ba3
 
 import {logo} from "../../img";
 import { HeaderBlock, HeaderNav, HeaderWrapper, LinkContrast, Wrapper } from "./header.styled";
 
-<<<<<<< HEAD
-export function Header() {
-    return (
-        <>
-        <HeaderStyled>
-            <Wrapper>
-                <HeaderWrapper>
-                <img src={logo} alt="Логотип. PsycologyTesting"/>
-                <nav>
-                    <HeaderList>
-                        <li><StyledLink href="#01">ТЕСТЫ</StyledLink></li>
-                        <li><StyledLink href="#02">ЛИЧНЫЙ КАБИНЕТ</StyledLink></li>
-                    </HeaderList>
-                </nav>
-                </HeaderWrapper>
-            </Wrapper>
-        </HeaderStyled>
-        </>
-    )
-=======
 export class Header extends React.Component{
     render(){
         return (
@@ -49,5 +25,4 @@ export class Header extends React.Component{
             </>
             )
     }
->>>>>>> dac0d4f27d8fb3092f3cfa8a6ada9e8b0eff8ba3
 }
