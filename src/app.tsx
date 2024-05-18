5efc5c3c02ef811aef6737148311b8f2cedee51c
dqriakh <dashakhan3009@mail.ru>
2024-05-18 10:03:37 +0300

    (#1) Error boundary

--- src/app.tsx
+++ src/app.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { PageRoutes } from './routes';
 
+import ErrorBoundary from "./components/error-boundary"
+
 import './css/style.css'
 import './css/record.css'
 
 const App = () => {
 
   return (
-    <BrowserRouter>
-      {/* <Global styles={globalStyles} /> */}
-      <PageRoutes />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        {/* <Global styles={globalStyles} /> */}
+        <PageRoutes />
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 
 };
--- src/routes.tsx
+++ src/routes.tsx
@@ -10,17 +10,15 @@ import Reg from './pages/registration'
 import Enter from './pages/enter'
 
 export const PageRoutes = () => (
-    // <ErrorBoundary>
-        <Routes>
-            <Route path={URLs.baseUrl} element={<Main />} />
-            {URLs.ui.main && <Route path={URLs.ui.main} element={<Main />} />}
-            {URLs.ui.lk && <Route path={URLs.ui.lk} element={<Lk />} />}
-            {URLs.ui.record && <Route path={URLs.ui.record} element={<Rec />} />}
-            {URLs.ui.testlist && <Route path={URLs.ui.testlist} element={<Testlist />} />}
-            {URLs.ui.registration && <Route path={URLs.ui.registration} element={<Reg />} />}
-            {URLs.ui.enter && <Route path={URLs.ui.enter} element={<Enter />} />}
+    <Routes>
+        <Route path={URLs.baseUrl} element={<Main />} />
+        {URLs.ui.main && <Route path={URLs.ui.main} element={<Main />} />}
+        {URLs.ui.lk && <Route path={URLs.ui.lk} element={<Lk />} />}
+        {URLs.ui.record && <Route path={URLs.ui.record} element={<Rec />} />}
+        {URLs.ui.testlist && <Route path={URLs.ui.testlist} element={<Testlist />} />}
+        {URLs.ui.registration && <Route path={URLs.ui.registration} element={<Reg />} />}
+        {URLs.ui.enter && <Route path={URLs.ui.enter} element={<Enter />} />}
 
-            <Route path="*" element={<h1>Page not found</h1>} />
-        </Routes>
-    // </ErrorBoundary>
+        <Route path="*" element={<h1>Page not found</h1>} />
+    </Routes>
 )
