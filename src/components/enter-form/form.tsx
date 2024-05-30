0404c3e4448f5bde613e5ccd93d00b6de088c65d
dqriakh <dashakhan3009@mail.ru>
2024-05-30 16:19:38 +0300

    registration, enter

--- src/components/enter-form/form.tsx
+++ src/components/enter-form/form.tsx
@@ -1,18 +1,105 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 
 import { StyledBg, StyledForm, StyledButton, StyledHeader, StyledInfo, StyledInput } from "./form.styled";
 
 export function Form(props) {
+
+  const [users, setUsers] = useState([]);
+useEffect(() => {
+  fetch('/api/users-data')
+      .then(response => response.json())
+      .then(data => {
+          setUsers(data);
+      })
+      .catch(error => {
+          console.error('Error fetching users:', error);
+      });
+}, []);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = {
+      email,
+      password
+    };
+
+    const findUser = users.find(user => user.eMail === email);
+
+    if (props.first === "Регистрация") {
+      if (!findUser) {
+        fetch('/api/submit-reg', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            alert("Регистрация прошла успешно!");
+          })
+          .catch((error) => {
+            console.error(error);
+            alert("Произошла ошибка.");
+          });
+      } else {
+          alert("Пользователь с таким почтовым адресом уже зарегистрирован.")
+      }
+    } else if(props.first === "Вход") {
+        if(findUser) {
+          if (findUser.password === password) {
+            fetch('/api/submit-enter', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(formData),
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(data);
+                alert("Вы успешно вошли в аккаунт!");
+              })
+              .catch((error) => {
+                console.error(error);
+                alert("Произошла ошибка.");
+              });
+          } else {
+            alert("Неверный пароль");
+          }
+        } else {
+          alert("Пользователь не найден. Пройдите регистрацию.")
+        }
+    }
+
+    // Очистка полей формы
+    setEmail('');
+    setPassword('');
+  };
+
   return (
     <StyledForm>
       <StyledBg>
         <StyledHeader>{props.first}</StyledHeader>
         <StyledInfo>
-          <StyledInput type="email" placeholder="Email" name="email" required/>
-          <StyledInput type="password" placeholder="Пароль" name="password" required/>
-          <StyledButton type="submit" form="id-info">{props.second}</StyledButton>
+          <StyledInput type="email" placeholder="Email" name="email" value={email} onChange={handleEmailChange} required/>
+          <StyledInput type="password" placeholder="Пароль" name="password" value={password} onChange={handlePasswordChange} required/>
+          <StyledButton type="submit" form="id-info" onClick={handleSubmit}>{props.second}</StyledButton>
         </StyledInfo>
         <Link to={props.href}>{props.text}</Link>
       </StyledBg>
--- stubs/api/index.js
+++ stubs/api/index.js
@@ -1,8 +1,13 @@
+const { response } = require('express');
 const router = require('express').Router();
 
 router.get('/tests-data', (request, response) => {
     response.send(require('../json/tests-data/success.json'))
   })
+
+router.get("/users-data", (request, response) => {
+    response.send(require("../json/users-data/success.json"))
+})
 
 router.get('/results-data', (request, response) => {
     response.send(require('../json/results-data/results.json'))
@@ -11,4 +16,19 @@ router.get('/results-data', (request, response) => {
 router.get('/records-data', (request, response) => {
     response.send(require('../json/records-data/records.json'))
   })
+
+  router.get('/users-data', (request, response) => {
+    response.send(require('../json/users-data/success.json'))
+  })
+
+  router.post('/submit-enter', (request, response) => {
+    const data = request.body; 
+    response.send({ success: true, message: "Вы успешно вошли в аккаунт!" });
+  });
+
+  router.post('/submit-reg', (request, response) => {
+    const data = request.body; 
+    response.send({ success: true, message: "Регистрация прошла успешно!" });
+  });
+
 module.exports = router;
