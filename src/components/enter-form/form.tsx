32463a3293d385c588f2346140c031781751ba03
sashunechko <alexandraaa.ha@gmail.com>
2024-05-31 11:02:40 +0300

    some fix success registration

--- src/components/enter-form/form.tsx
+++ src/components/enter-form/form.tsx
@@ -1,13 +1,29 @@
 import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
+
 import PropTypes from 'prop-types';
 import { URLs } from "../../__data__/urls";
-
-import { StyledBg, StyledForm, StyledButton, StyledHeader, StyledInfo, StyledInput } from "./form.styled";
-
+{/*import Lottie from 'react-lottie';
+import {Tip1} from '../../components/record/info/info.styled';
+import {StyledLink, StyledTip} from '../../components/result/result.styled';*/}
+import { StyledBg, StyledForm, StyledButton, StyledHeader, StyledInfo, StyledInput } from "./form.styled"; 
+import * as animationData from '../../../src/assets/succes-submit.json';
+import { useNavigate } from 'react-router-dom';
 export function Form(props) {
 
   const [users, setUsers] = useState([]);
+  const [isSuccess,setSuccess] = useState(false);
+  const [success] = useState(true);
+  const navigate = useNavigate();
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: true, 
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
 useEffect(() => {
   fetch(`${URLs.api.main}/users-data`)
       .then(response => response.json())
@@ -52,7 +68,10 @@ useEffect(() => {
           .then((response) => response.json())
           .then((data) => {
             console.log(data);
-            alert("Регистрация прошла успешно!");
+            setSuccess(true);
+            if (success) {
+              navigate(URLs.ui.lk); 
+          }
           })
           .catch((error) => {
             console.error(error);
@@ -74,7 +93,10 @@ useEffect(() => {
               .then((response) => response.json())
               .then((data) => {
                 console.log(data);
-                alert("Вы успешно вошли в аккаунт!");
+                setSuccess(true);
+                if (success) {
+                  navigate(URLs.ui.lk); 
+              }
               })
               .catch((error) => {
                 console.error(error);
@@ -88,19 +110,29 @@ useEffect(() => {
         }
     }
 
-    // Очистка полей формы
-    setEmail('');
-    setPassword('');
+  
   };
 
   return (
     <StyledForm>
+      {/* {isSuccess &&
+      <div>
+      <Lottie options={defaultOptions}
+        height={400}
+        width={400}/>
+        <Tip1>
+        <StyledTip>вы успешно вошли</StyledTip>
+        <StyledLink to={URLs.ui.lk}>личный кабинет</StyledLink>
+        </Tip1>
+      </div> } */}
       <StyledBg>
         <StyledHeader>{props.first}</StyledHeader>
         <StyledInfo>
+        <form onSubmit={handleSubmit}>
           <StyledInput type="email" placeholder="Email" name="email" value={email} onChange={handleEmailChange} required/>
           <StyledInput type="password" placeholder="Пароль" name="password" value={password} onChange={handlePasswordChange} required/>
-          <StyledButton type="submit" onClick={handleSubmit}>{props.second}</StyledButton>
+          <StyledButton type="submit" >{props.second}</StyledButton>
+        </form>
         </StyledInfo>
         <Link to={props.href}>{props.text}</Link>
       </StyledBg>
