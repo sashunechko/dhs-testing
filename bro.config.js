c12b7e7893908ebb1e74ad45608400b7f75c44d1
sashunechko <alexandraaa.ha@gmail.com>
2024-11-30 16:01:23 +0300

    ijl -> brojs

---
+++ bro.config.js
@@ -0,0 +1,30 @@
+const pkg = require('./package')
+
+module.exports = {
+  apiPath: 'stubs/api',
+  webpackConfig: {
+    output: {
+      publicPath: `/static/${pkg.name}/${process.env.VERSION || pkg.version}/`
+    }
+  },
+  /* use https://kc.admin.inno-js.ru/ to create config, navigations and features */
+  navigations: {
+    "dhs-testing.main": "/dhs-testing",
+    "dhs-testing.testlist": "/testlist",
+    "dhs-testing.first": "/main",
+    "dhs-testing.lk": "/lk",
+    "dhs-testing.record": "/record",
+    "dhs-testing.enter": "/enter",
+    "dhs-testing.registration": "/registration",
+    "dhs-testing.q": "/q",
+    "dhs-testing.result": "/result"
+  },
+  features: {
+    'dhs-testing': {
+      // add your features here in the format [featureName]: { value: string }
+    },
+  },
+  config: {
+    "dhs-testing.api": "/api"
+  },
+}
--- src/__data__/urls.ts
+++ src/__data__/urls.ts
@@ -1,20 +1,19 @@
-import { getNavigationsValue, getConfigValue } from '@ijl/cli';
-
-const baseUrl = getNavigationsValue('dhs-testing.main');
+import { getNavigationValue, getConfigValue } from '@brojs/cli';
+const baseUrl = getNavigationValue('dhs-testing.main');
 
 export const URLs = {
-  baseUrl,
-  ui: {
-    main: getNavigationsValue('dhs-testing.first') && `${baseUrl}${getNavigationsValue('dhs-testing.first')}`,
-    testlist: getNavigationsValue('dhs-testing.testlist') && `${baseUrl}${getNavigationsValue('dhs-testing.testlist')}`,
-    enter: getNavigationsValue('dhs-testing.enter') && `${baseUrl}${getNavigationsValue('dhs-testing.enter')}`,
-    registration: getNavigationsValue('dhs-testing.registration') && `${baseUrl}${getNavigationsValue('dhs-testing.registration')}`,
-    record: getNavigationsValue('dhs-testing.record') && `${baseUrl}${getNavigationsValue('dhs-testing.record')}`,
-    q: getNavigationsValue('dhs-testing.q') && `${baseUrl}${getNavigationsValue('dhs-testing.q')}`,
-    lk: getNavigationsValue('dhs-testing.lk') && `${baseUrl}${getNavigationsValue('dhs-testing.lk')}`,
-    result: getNavigationsValue('dhs-testing.result') && `${baseUrl}${getNavigationsValue('dhs-testing.result')}`
-  },
-  api: {
-    main: getConfigValue('dhs-testing.api')
-  },
-}
+    baseUrl,
+    ui: {
+      main: getNavigationValue('dhs-testing.first') && `${baseUrl}${getNavigationValue('dhs-testing.first')}`,
+      testlist: getNavigationValue('dhs-testing.testlist') && `${baseUrl}${getNavigationValue('dhs-testing.testlist')}`,
+      enter: getNavigationValue('dhs-testing.enter') && `${baseUrl}${getNavigationValue('dhs-testing.enter')}`,
+      registration: getNavigationValue('dhs-testing.registration') && `${baseUrl}${getNavigationValue('dhs-testing.registration')}`,
+      record: getNavigationValue('dhs-testing.record') && `${baseUrl}${getNavigationValue('dhs-testing.record')}`,
+      q: getNavigationValue('dhs-testing.q') && `${baseUrl}${getNavigationValue('dhs-testing.q')}`,
+      lk: getNavigationValue('dhs-testing.lk') && `${baseUrl}${getNavigationValue('dhs-testing.lk')}`,
+      result: getNavigationValue('dhs-testing.result') && `${baseUrl}${getNavigationValue('dhs-testing.result')}`
+    },
+    api: {
+      main: getConfigValue('dhs-testing.api')
+    },
+  }
